e8217572a08c63b2ba7984ae7261e8447aef719c
Elaine <mcunha@g.harvard.edu>
2021-05-12 00:59:48 -0400

    adding finite cutoff graphs

--- run_finite_swarm.cc
+++ run_finite_swarm.cc
@@ -13,27 +13,21 @@
 
 int main() {
     
-    double J = 0.5, K = 0.5, N = 250;
-    double r;
+    double J = 0.1, K = -1, N = 100;
+    double r[4];
+    r[0]=-1; r[1]=2; r[2]=1.;r[3]=0.5;
     
-    for (int i=0; i<1; i++) {
-    
-//        if (i==0) {
-//            r=-1;
-//        } else {
-//            r = 0.5*i;
-//        }
-        r = 0.5;
+    for (int i=0; i<4; i++) {
         
-        std::unique_ptr<swarm_sol> o(new swarm_sol(J,K,N,r));
-        o->filename = "figs_data/finite_swarm_r"+std::to_string(r)+".txt";
+        std::unique_ptr<swarm_sol> o(new swarm_sol(J,K,N,r[i]));
+        o->filename = "figs_data/finite_swarm_r"+std::to_string(r[i])+".txt";
         o->solve_fixed(250.,1e-6,true,500);
         
-        std::unique_ptr<swarm_sol_3d> p(new swarm_sol_3d(J,K,N,r));
-        p->filename = "figs_data/finite_swarm3d_r"+std::to_string(r)+".txt";
+        std::unique_ptr<swarm_sol_3d> p(new swarm_sol_3d(J,K,N,r[i]));
+        p->filename = "figs_data/finite_swarm3d_r"+std::to_string(r[i])+".txt";
         p->solve_fixed(250.,1e-6,true,500);
         
-        printf("Finished with r=%g\n",r);
+        printf("Finished with r=%g\n",r[i]);
         
     }
     
